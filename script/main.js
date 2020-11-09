67323bbdb9007a761d0fc6fdacdd1b2251c45eee
curviture <gevghazaryan@aol.com>
2020-11-10 01:22:00 +0400

    all resolutions alpha 1.2 version commit

--- script/main.js
+++ script/main.js
@@ -1,7 +1,16 @@
 let triggerButton = document.getElementById('menu-open');
-let menu = document.getElementById('nav__menu')
+let menu = document.getElementById('nav__menu');
+let closeButton = document.getElementById('close__button');
+
+console.log(closeButton)
 
 triggerButton.addEventListener('click', () => {
     menu.classList.toggle('open')
     console.log(menu,menu.classList)
+})
+
+
+closeButton.addEventListener('click', () => {
+    console.log('i hit it')
+    // menu.classList.toggle('open')
 })
