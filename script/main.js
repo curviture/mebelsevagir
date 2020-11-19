e0e7d9ab65c5a29cbaae0d14ee70eebe393cc08a
curviture <gevghazaryan@aol.com>
2020-11-20 00:24:30 +0400

    all resolutions alpha 2.05 version fixing bugs

--- script/main.js
+++ script/main.js
@@ -2,8 +2,6 @@ let triggerButton = document.getElementById('menu-open');
 let menu = document.getElementById('nav__menu');
 let closeButton = document.getElementById('close__button');
 
-console.log(closeButton)
-
 triggerButton.addEventListener('click', () => {
     menu.classList.toggle('open')
     console.log(menu,menu.classList)
