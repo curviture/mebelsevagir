234320a37cced85f0894479bd57e89a2a3d2233c
curviture <gevghazaryan@aol.com>
2020-12-04 21:26:45 +0400

    finished navbar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,51 @@
+
 $('#call-from-navbar').modaal({
     content_source: '#js-call-navbar'
-})
+});
+
+//moving items in case of small screen
+(function() {
+    let width = screen.availWidth;
+    let headerMenuLink = document.querySelectorAll('.menu--header .menu__link');
+    console.log(width)
+    if(width <= 760) {
+        headerMenuLink.forEach(function(item) {
+            item.classList.remove('animated-underline')
+        })
+    }
+    if(width > 760) {
+        headerMenuLink.forEach(function(item) {
+            item.classList.add('animated-underline')
+        })
+    }
+}());
+
+//closing and opening menu on small screen(max-width < 770px)
+(function() {
+    let trigger = document.getElementById('js-header-trigger');
+    let closer = document.getElementById('js-menu-link-closer');
+    let menu = document.getElementById('js-header-menu');
+    trigger.addEventListener('click', function() {
+        menu.classList.toggle('open')
+    })
+    closer.addEventListener('click', function(event) {
+        event.preventDefault()
+        menu.classList.remove('open');
+    })
+}());
+
+(function() {
+    let items = document.querySelectorAll('.js-move');
+    console.log('hello kitt')
+    console.log(items)
+    items.forEach(function(item) {
+        if(screen.availWidth <= +item.dataset.screen) {
+            let moveto = document.getElementById(item.dataset.moveto);
+            let clone = item.cloneNode(true);
+            moveto.appendChild(clone)
+            item.classList.add('hidden')
+        }
+
+    })
+}())
+
